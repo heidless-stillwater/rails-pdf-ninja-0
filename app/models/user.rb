e421de63c08207fb23d3475c8cc840409871256a
heidless <rob.lockhart@yahoo.co.uk>
2024-07-15 09:49:39 +0100

    init articles model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
 
   has_one :payment
   accepts_nested_attributes_for :payment
+
   has_many :images
+  has_many :articles
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_07_14_165213) do
+ActiveRecord::Schema.define(version: 2024_07_15_080602) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.text "title"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
 
   create_table "images", force: :cascade do |t|
     t.string "name"
@@ -48,5 +57,6 @@ ActiveRecord::Schema.define(version: 2024_07_14_165213) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "articles", "users"
   add_foreign_key "images", "users"
 end
