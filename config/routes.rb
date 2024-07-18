ad0ea6620e9010674e2422823b716e002edf3cf0
heidless <rob.lockhart@yahoo.co.uk>
2024-07-18 15:38:32 +0100

    attempt 0

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
+
+  devise_for :users, :controllers => { :registrations => 'registrations' }
+  resources :users, only: [:index, :show, :edit, :update] do
+    member do 
+      delete 'delete_image/:image_id', action: 'delete_image', as: 'delete_image'
+    end
+  end
+
   resources :images
   resources :pdfs
   resources :articles 
   # resources :articles, only: [:show, :index, :new, :create, :edit, :destroy]
 
-  devise_for :users, :controllers => { :registrations => 'registrations' }
   root 'welcome#index' 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
