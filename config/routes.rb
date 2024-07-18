faada18417bcf212f497a9488f7410f8a0272ab6
heidless <rob.lockhart@yahoo.co.uk>
2024-07-18 09:56:06 +0100

    reset successful

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  resources :images
   resources :pdfs
   resources :articles 
   # resources :articles, only: [:show, :index, :new, :create, :edit, :destroy]
 
-  resources :images
   devise_for :users, :controllers => { :registrations => 'registrations' }
   root 'welcome#index' 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
